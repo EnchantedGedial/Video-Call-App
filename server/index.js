f468286a77e7fba0e00f8f7b878240c5a59b1173
Jayanth <jay.codmik.18@gmail.com>
2023-07-25 21:52:46 +0530

    2nd commit

--- server/index.js
+++ server/index.js
@@ -1,7 +1,22 @@
 const {Server}= require("socket.io")
 
-const io =new Server(8000)
+const io =new Server(8000,{
+    cors :true
+})
+
+
+const emailToSocketIdMap= new Map()
+const socketToEmailMap=new Map()
 
 io.on("connection",(socket)=>{
-    console.log(`Socket Connected`,socket.id)
+    console.log(`Socket Connected:`,socket.id)
+    socket.on('room:join',data=>{
+       const{email,room} =data;
+       emailToSocketIdMap.set(email,socket.id)
+       socketToEmailMap.set(socket.id,email)
+       io.to(room).emit('user:joined',{email,id:socket.id})
+       socket.join(room)
+       io.to(socket.id).emit('room:join',data)
+    
+    })
 })
